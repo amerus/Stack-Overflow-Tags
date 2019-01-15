8e20890acdcbea3315546b3e2e5ed409708ed4fe
amerus <sergey.motorny@vanderbilt.edu>
2019-01-15 11:59:55 -0600

    with instructions in css

--- shinyapp/ui.R
+++ shinyapp/ui.R
@@ -4,6 +4,22 @@ shinyUI(
     dashboardPage(
         dashboardHeader(title = 'Stack Overflow Tags'),
         dashboardSidebar(
+          tags$style(HTML("
+              @import url('//fonts.googleapis.com/css?family=Lobster|Cabin:400,700');
+                  h3 {
+                        font-family: 'Lobster';
+                        font-weight: 10;
+                        line-height: 1.1;
+                        padding: 0px 10px 0px 10px;
+                      }
+                  h5 {
+                        font-family: 'Courier';
+                        font-weight: 6;
+                        padding: 0px 10px 0px 10px;
+                      }        
+                          ")),
+          tags$h3('Visual Tag Analysis of Russian and English Stack Overflow Communities'),
+          tags$p(),
                 sliderInput("selectYear", 
                             label = "Years:",
                             min = min(as.numeric(dropDownYears)),
@@ -18,13 +34,14 @@ shinyUI(
                             max = 100,
                             value = 5
                 ),
-                         
                 selectInput("selectTag",
                             label = "Additional Tags",
                             choices = dropDownTags,
                             multiple = TRUE,
-                            selected = unlist(head(dropDownTags, n = 5)))
-                         ),
+                            selected = unlist(head(dropDownTags, n = 5))),
+          tags$h5('Brief instructions: Use controls on the left to interact with the graphs on the right.
+                  Right panel has three graphs separated by tabs. Use the same controls to interact with all of them.')
+                ),
         dashboardBody(
            tabsetPanel(
              tabPanel(
@@ -39,7 +56,7 @@ shinyUI(
               title = "Russian-Specific Tags (2014-2018)",
               plotlyOutput("rusSpecific", height = 600)
             )
-            )
+           )
          )
       )
    )
